7397d0cb1cc4f46c36d80e843679ce22c2586e1d
Cinguinique Erquette <erquette@ualberta.ca>
2023-02-17 20:41:59 -0700

    Added naming system method and name attribute to QR_Code class

--- QR_Code_Hunter/app/src/main/java/com/example/qr_code_hunter/QR_Code.java
+++ QR_Code_Hunter/app/src/main/java/com/example/qr_code_hunter/QR_Code.java
@@ -2,4 +2,44 @@ package com.example.qr_code_hunter;
 
 public class QR_Code {
 
+    String code_name = "";
+
+    /**
+     * This creates a unique name corresponding to one QR code using the first 10 bits of the binary value
+     * @param binary
+     *      This is the binary value of the SHA-256 hash of the code
+     */
+    public void setName(String binary) {
+        char bin[] = binary.toCharArray();
+        String[][] namesArray = new String[][] {
+            new String[] {"Red", "Jet"},
+            new String[] {"Sea", "Bay"},
+            new String[] {"Car", "Gas"},
+            new String[] {"Art", "Era"},
+            new String[] {"Owl", "Bat"},
+            new String[] {"Jaw", "Eye"},
+            new String[] {"Oak", "Log"},
+            new String[] {"Flu", "Ice"},
+            new String[] {"Mud", "Oil"},
+            new String[] {"Saw", "Axe"}
+        };
+    }
+
+    for (int i = 0; i < 10; i++) {
+        if (bin[i] == '1') {
+            code_name = code_name.concat(namesArray[i][1]);
+        } else {
+            code_name = code_name.concat(namesArray[i][0]);
+        }
+    }
+
+    /**
+     * This returns the name of a code
+     * @return
+     *      Returns a string-type
+     */
+    public String getName() {
+        return code_name;
+    }
+
 }
