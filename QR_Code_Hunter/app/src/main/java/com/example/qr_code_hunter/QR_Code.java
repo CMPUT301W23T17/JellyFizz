f846fa10890b0760be68fa2521e20bfdff95d081
Daniel Akanmu <inioluwa.akanmu@gmail.com>
2023-02-19 18:37:32 -0700

    visual representation

--- QR_Code_Hunter/app/src/main/java/com/example/qr_code_hunter/QR_Code.java
+++ QR_Code_Hunter/app/src/main/java/com/example/qr_code_hunter/QR_Code.java
@@ -2,4 +2,11 @@ package com.example.qr_code_hunter;
 
 public class QR_Code {
 
+    public String getVisualRep(String binary){
+        char bin[] = binary.toCharArray();
+
+        String rep = " ---- /n ----";
+
+        return rep;
+    }
 }
