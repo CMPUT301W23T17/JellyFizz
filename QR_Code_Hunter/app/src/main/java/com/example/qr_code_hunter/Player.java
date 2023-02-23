18a3c6f35176ce4eb8d7ea9b6085ca0415ac24d8
Cinguinique Erquette <erquette@ualberta.ca>
2023-02-22 17:21:44 -0700

    Added privacy and total code methods

--- QR_Code_Hunter/app/src/main/java/com/example/qr_code_hunter/Player.java
+++ QR_Code_Hunter/app/src/main/java/com/example/qr_code_hunter/Player.java
@@ -1,5 +1,34 @@
 package com.example.qr_code_hunter;
 
-public class Player {
+import java.util.ArrayList;
 
+public class Player {
+    ArrayList<QR_Code> myQR_Codes = new ArrayList<QR_Code>();
+    Integer total_codes_scanned;
+    Boolean privacy = false; // false = not hidden, true = hidden -- default is shown
+
+    /**
+     * This changes the visibility of user information (email & phone num.)
+     * @param visibility
+     *      true -> hide details from other players, false -> details visible to other players
+     */
+    public void setPrivacy(Boolean visibility) {
+        this.privacy = visibility;
+    }
+
+    /**
+     * This counts the number of QR_Code objects in the array
+     */
+    public void totalQR_Codes() {
+        total_codes_scanned = myQR_Codes.size();
+    }
+
+    /**
+     * This is a getter method for the number of QR Codes scanned
+     * @return
+     *      returns the total number of QR codes scanned
+     */
+    public int getTotalQR_Code() {
+        return total_codes_scanned;
+    }
 }
