839b2f61ccb0dee35c1ad6b3bb529a18e6779123
Cinguinique Erquette <erquette@ualberta.ca>
2023-03-03 01:40:48 -0700

    Fragments after scanning a new code

---
+++ QR_Code_Hunter/app/src/main/java/com/example/qr_code_hunter/new_code_activity.java
@@ -0,0 +1,14 @@
+package com.example.qr_code_hunter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class new_code_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_code);
+    }
+}
