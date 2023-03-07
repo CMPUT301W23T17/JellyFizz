a812097710c32901c5835b5e59d70632057e4d23
hrishit-boop <73561430+hrishit-boop@users.noreply.github.com>
2023-03-07 13:33:59 -0700

    Update camera.java

--- QR_Code_Hunter/app/src/main/java/com/example/qr_code_hunter/camera.java
+++ QR_Code_Hunter/app/src/main/java/com/example/qr_code_hunter/camera.java
@@ -1,81 +1,105 @@
 package com.example.qr_code_hunter;
 
-public class Camera {
+package com.example.camera;
 
-    private static final int REQUEST_CAMERA_PERMISSION = 200;
-    private static final int REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION = 201;
-    private static final String[] PERMISSIONS = {
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class camera {
+    private static final String TAG = "Camera";
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_PERMISSIONS = 2;
+    private static final String[] REQUIRED_PERMISSIONS = new String[]{
             Manifest.permission.CAMERA,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
     };
-    private Activity mActivity;
-    private CameraView mCameraView;
-    private boolean mHasCameraPermission;
-    private boolean mHasWriteExternalStoragePermission;
+    private final Activity activity;
+    private Uri photoUri;
+    private OnPictureTakenListener onPictureTakenListener;
 
-    public Camera(Activity activity, CameraView cameraView) {
-        mActivity = activity;
-        mCameraView = cameraView;
+    public interface OnPictureTakenListener {
+        void onPictureTaken(Uri photoUri);
     }
 
-    public void checkPermissions() {
-        mHasCameraPermission = ActivityCompat.checkSelfPermission(mActivity, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
-        mHasWriteExternalStoragePermission = ActivityCompat.checkSelfPermission(mActivity, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
-
-        if (!mHasCameraPermission || !mHasWriteExternalStoragePermission) {
-            ActivityCompat.requestPermissions(mActivity, PERMISSIONS, REQUEST_CAMERA_PERMISSION);
-        }
-    }
-
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_CAMERA_PERMISSION:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    mHasCameraPermission = true;
-                }
-                if (grantResults.length > 1 && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-                    mHasWriteExternalStoragePermission = true;
-                }
-                if (mHasCameraPermission && mHasWriteExternalStoragePermission) {
-                    startCamera();
-                } else {
-                    Toast.makeText(mActivity, "Camera and storage permission required", Toast.LENGTH_SHORT).show();
-                }
-                break;
-        }
-    }
-
-    private void startCamera() {
-        mCameraView.setLifecycleOwner(mActivity);
-        mCameraView.setCaptureMode(CameraView.CaptureMode.IMAGE);
-        mCameraView.addCameraListener(new CameraListener() {
-            @Override
-            public void onPictureTaken(@NonNull PictureResult result) {
-                super.onPictureTaken(result);
-                savePicture(result);
-            }
-        });
+    public camera(Activity activity) {
+        this.activity = activity;
     }
 
     public void takePicture() {
-        if (mHasCameraPermission && mHasWriteExternalStoragePermission) {
-            mCameraView.takePicture();
+        if (allPermissionsGranted()) {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(activity.getPackageManager()) != null) {
+                File photoFile = null;
+                try {
+                    photoFile = createImageFile();
+                } catch (Exception ex) {
+                    Log.e(TAG, "Error occurred while creating the File", ex);
+                }
+                if (photoFile != null) {
+                    photoUri = Uri.fromFile(photoFile);
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                    activity.startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
         } else {
-            Toast.makeText(mActivity, "Camera and storage permission required", Toast.LENGTH_SHORT).show();
+            ActivityCompat.requestPermissions(activity, REQUIRED_PERMISSIONS, REQUEST_PERMISSIONS);
         }
     }
 
-    private void savePicture(PictureResult result) {
-        if (!mHasWriteExternalStoragePermission) {
-            ActivityCompat.requestPermissions(mActivity, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION);
-        } else {
-            File file = new File(mActivity.getExternalFilesDir(null), "picture.jpg");
-            result.toFile(file, ignored -> {
-                Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-                mediaScanIntent.setData(Uri.fromFile(file));
-                mActivity.sendBroadcast(mediaScanIntent);
-                Toast.makeText(mActivity, "Picture saved to " + file.getAbsolutePath(), Toast.LENGTH_SHORT).show();
-            });
+    public void setOnPictureTakenListener(OnPictureTakenListener onPictureTakenListener) {
+        this.onPictureTakenListener = onPictureTakenListener;
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                takePicture();
+            } else {
+                Log.e(TAG, "Permissions not granted: " + REQUIRED_PERMISSIONS.toString());
+            }
         }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            if (photoUri != null) {
+                if (onPictureTakenListener != null) {
+                    onPictureTakenListener.onPictureTaken(photoUri);
+                }
+            }
+        } else {
+            Log.e(TAG, "Error occurred while taking photo");
+        }
+    }
+
+    private boolean allPermissionsGranted() {
+        for (String permission : REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private File createImageFile() throws Exception {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        return File.createTempFile(imageFileName, ".jpg", storageDir);
     }
 }
