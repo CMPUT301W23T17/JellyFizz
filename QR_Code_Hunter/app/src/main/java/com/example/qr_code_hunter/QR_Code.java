f0970736a4497deb653ce2e82cd82388bd1522d7
kyle-quach <109721786+kyle-quach@users.noreply.github.com>
2023-03-06 20:03:10 -0700

    22222222222222

--- QR_Code_Hunter/app/src/main/java/com/example/qr_code_hunter/QR_Code.java
+++ QR_Code_Hunter/app/src/main/java/com/example/qr_code_hunter/QR_Code.java
@@ -1,9 +1,24 @@
 package com.example.qr_code_hunter;
 
-public class QR_Code {
-    String code_name = "";
-    private int score;
+import java.util.Map;
 
+public class QR_Code {
+    private String code_name = "";
+    private Integer score;
+
+
+
+
+
+
+    private static Map<Character, Integer> alphabetPoints = Map.of(
+            'a', 10,
+            'b', 11,
+            'c', 12,
+            'd', 13,
+            'e', 14,
+            'f', 15
+    );
 
 
 
@@ -45,6 +60,66 @@ public class QR_Code {
         }
     }
 
+    /**
+     * @param hashString this is the hash of the QR_Code scanned representation
+     * @return this methods returns the score of the QR_Code based on the scoring system on eclass
+     */
+    public static void scoreQR_Code(String hashString) {
+        int score = 0;
+        char currentChar = hashString.charAt(0);
+        int count = 0;
+
+        for (int i = 1; i < hashString.length(); i++) {
+
+            if (hashString.charAt(i) == currentChar) {
+                count++;
+            } else {
+                int iterationScore = 0;
+
+                if (Character.isDigit(currentChar)) {
+                    int repeatedInt = currentChar - 48;
+
+                    if (repeatedInt == 0) {
+                        iterationScore = (int) Math.pow(20, count);
+                    } else {
+                        iterationScore = (int) Math.pow(repeatedInt, count);
+                    }
+                } else {
+                    int valueOfChar = alphabetPoints.get(currentChar);
+                    iterationScore = (int) Math.pow(valueOfChar, count);
+                }
+                if (count > 0) score += iterationScore;
+                currentChar = hashString.charAt(i);
+                count = 0;
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     /**
@@ -52,24 +127,18 @@ public class QR_Code {
      * @return
      *      Returns a string-type
      */
-    public String getName() {
-        return code_name;
-    }
+    public String getName() {return code_name;}
+    /**
+     * This returns the visual score of a code
+     * @return
+     *      Returns a integer-type
+     */
+    public Integer getScore() {return score;}
     /**
      * This returns the visual reprsentation of a code
      * @return
      *      Returns a string-type
      */
-    public String getVisualRep() {
-        return code_name;
-    }
-    /**
-     * This returns the visual reprsentation of a code
-     * @return
-     *      Returns a string-type
-     */
-    public String getVisualRep() {
-        return code_name;
-    }
+    public String getVisualRep() {return code_name;}
 
 }
