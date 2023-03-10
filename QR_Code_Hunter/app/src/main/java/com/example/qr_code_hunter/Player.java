c41d112f72300b6bd66a8b0522a49063b5c76f4a
Cinguinique <erquette@ualberta.ca>
2023-03-10 15:42:39 -0700

    modified Player and added PlayerProfile based on UML

--- QR_Code_Hunter/app/src/main/java/com/example/qr_code_hunter/Player.java
+++ QR_Code_Hunter/app/src/main/java/com/example/qr_code_hunter/Player.java
@@ -1,51 +1,140 @@
 package com.example.qr_code_hunter;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+
 import java.util.ArrayList;
 
 public class Player {
-    private ArrayList<QRCode> myQR_Codes = new ArrayList<QRCode>();
+    protected ArrayList<DocumentReference> myQrCodes = new ArrayList<>();
+    protected String uniqueUsername;
+    protected PlayerProfile profileInfo;
+    protected int totalCodeScanned;
+    protected int totalScore;
+    protected int rank;
+
+    public Player(String phone, String email, String username, Boolean privacy, ArrayList<DocumentReference> codeScanned, int score, int rank) {
+        this.profileInfo = new PlayerProfile(phone, email, privacy);
+        this.uniqueUsername = username;
+        this.myQrCodes = codeScanned; // Document references of player's QR codes in db
+        this.totalScore = score;
+        this.rank = rank;
+    }
 
     /**
-     * Returns QR Code which has the highest score
-     * @return a  QR code
+     * This finds the QrCode with the lowest score
+     * @return
+     *      Returns the hash string of the QrCode with the lowest score
      */
-    public QRCode highestScoreQR_Code(){
-        QRCode result = null;
-        for (QRCode item : myQR_Codes) {
-            if ( result == null){
-                result = item;
-            }
-            else if ( item.getScore() > result.getScore()){
-                result = item;
-            }
+    public String lowestScoreQrCode() {
+        final int[] lowest = {-1};
+        final String[] lowestQr = {null};
+        for (DocumentReference hashString: myQrCodes) {
+            hashString.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    int qrScore = documentSnapshot.getLong("Score").intValue();
+                    if (lowest[0] < 0) {
+                        lowest[0] = qrScore;
+                        lowestQr[0] = hashString.toString();
+                    }
+                    else if (lowest[0] > 0 && qrScore < lowest[0]) {
+                        lowest[0] = qrScore;
+                        lowestQr[0] = hashString.toString();
+                    }
+                }
+            });
         }
-        return result;
+        return lowestQr[0];
     }
+
     /**
-     * Returns QR Code which has the lowest score
-     * @return a  QR code
+     * This finds the QrCode with the highest score
+     * @return
+     *      Returns the hash string of the QrCode with the highest score
      */
-    public QRCode lowestScoreQR_Code(){
-        QRCode result = null;
-        for (QRCode item : myQR_Codes) {
-            if ( result == null){
-                result = item;
-            }
-            else if ( item.getScore() < result.getScore()){
-                result = item;
-            }
+    public String highestScoreQrCode() {
+        final int[] highest = {-1};
+        final String[] highestQr = {null};
+        for (DocumentReference hashString: myQrCodes) {
+            hashString.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    int qrScore = documentSnapshot.getLong("Score").intValue();
+                    if (highest[0] < 0) {
+                        highest[0] = qrScore;
+                        highestQr[0] = hashString.toString();
+                    }
+                    else if (highest[0] > 0 && qrScore > highest[0]) {
+                        highest[0] = qrScore;
+                        highestQr[0] = hashString.toString();
+                    }
+                }
+            });
         }
-        return result;
+        return highestQr[0];
     }
+
     /**
-     * Returns the sum of QR Code score
-     * @return integer
+     * This sums up the scores of all QrCodes the player has scanned
+     * @return
+     *      Returns the total score of the QrCodes scanned
      */
-    public Integer sumScoreQR_Code(){
-        Integer result = 0;
-        for (QRCode item : myQR_Codes) {
-            result = result + item.getScore();
+    public void calculateScore() {
+        for (DocumentReference hashString: myQrCodes) {
+            hashString.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    int qrScore = documentSnapshot.getLong("Score").intValue();
+                    totalScore += qrScore;
+                }
+            });
         }
-        return result;
-    };
+    }
+
+    /**
+     * This returns the total amount of QrCode scanned by the player
+     * @return
+     *      Returns an integer
+     */
+    public int getTotalCodeScanned() {
+        return myQrCodes.size();
+    }
+
+    /**
+     * This returns the total score of the player
+     * @return
+     *      Returns an integer
+     */
+    public int getTotalScore() {
+        return totalScore;
+    }
+
+    /**
+     * This returns the rank of the player compared to other players based on the highest scoring QrCode
+     * @return
+     *      Returns an integer
+     */
+    public int getRank() {
+        return rank;
+    }
+
+    /**
+     * This returns the username of the player
+     * @return
+     *      Returns a string
+     */
+    public String getUsername() {
+        return uniqueUsername;
+    }
+
+    /**
+     * This returns the player profile of the player
+     * @return
+     *      Returns a PlayerProfile type
+     */
+    public PlayerProfile getProfileInfo() {
+        return profileInfo;
+    }
 }
