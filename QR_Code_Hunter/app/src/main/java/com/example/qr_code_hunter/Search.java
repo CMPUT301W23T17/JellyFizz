5ee7bdf5b6357ae8f6463ae5270c4e071b02de96
Daniel Akanmu <inioluwa.akanmu@gmail.com>
2023-03-11 20:42:00 -0700

    saving

--- QR_Code_Hunter/app/src/main/java/com/example/qr_code_hunter/Search.java
+++ QR_Code_Hunter/app/src/main/java/com/example/qr_code_hunter/Search.java
@@ -1,34 +1,43 @@
 package com.example.qr_code_hunter;
 
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.os.Bundle;
 import android.util.Log;
-import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import androidx.fragment.app.Fragment;
 
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldPath;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.ArrayList;
-import java.util.List;
 
-import io.grpc.Context;
-
-public class Search {
-    FirebaseFirestore db;
-    CollectionReference player = db.collection("Players");
+public class Search extends AppCompatActivity {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    final CollectionReference player = db.collection("Players");
     CollectionReference codes = db.collection("QrCodes");
 
     private ListView listView;
     private ArrayAdapter adapter;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.searching);
+
+    }
+
+
+
+    /**
+     * This method searches for the players with the username the user searches for
+     * @param string this is the username the user searched
+     */
     public void searchPlayer(String string){
         Query query = player.whereGreaterThanOrEqualTo(FieldPath.documentId(), string)
                 .orderBy(FieldPath.documentId())
@@ -38,25 +47,21 @@ public class Search {
             if (task.isSuccessful()) {
                 ArrayList<String> usernames = new ArrayList<>();
                 for (QueryDocumentSnapshot document : task.getResult()) {
-                    String documentId = document.getId();
-                    if (documentId.startsWith(string)) {
-                        usernames.add(documentId);
+                    String id = document.getId() ;
+                    if (id.startsWith(string)) {
+                        usernames.add(id + ": " + document.getLong("score") + "pts");
                     }
                 }
 
-                listView = listView.findViewById(R.id.search_list);
-                adapter = new ArrayAdapter<>(this, R.layout.searching_context, usernames);
-                listView.setAdapter(adapter);
-
-
+                listView = findViewById(R.id.search_list);
+//                adapter = new ArrayAdapter<>(this, R.layout.searching_context, usernames);
+//                listView.setAdapter(adapter);
 
             } else {
                 // Handle errors here.
                 Log.e("TAG", "Error getting documents: ", task.getException());
             }
         });
-
-
     }
 
 
