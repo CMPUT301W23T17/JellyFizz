9ec545d66aace9033915f53a7c31f85a2cc9dce3
Daniel Akanmu <inioluwa.akanmu@gmail.com>
2023-03-11 23:49:46 -0700

    saving

--- QR_Code_Hunter/app/src/main/java/com/example/qr_code_hunter/Search.java
+++ QR_Code_Hunter/app/src/main/java/com/example/qr_code_hunter/Search.java
@@ -18,9 +18,17 @@ public class Search{
     CollectionReference codes = db.collection("QrCodes");
     ArrayList<String> usernames = new ArrayList<>();
 
+
+
     public interface SearchPlayerCallback {
         void onSearchPlayerComplete(ArrayList<String> usernames);
     }
+
+    /**
+     * Searches for the username the user inputs
+     * @param string username the user inputs
+     * @param callback stops the listener when task is complete
+     */
     public void searchPlayer(String string, SearchPlayerCallback callback){
         ArrayList<String> usernames = new ArrayList<>();
         Query query = player.whereGreaterThanOrEqualTo(FieldPath.documentId(), string)
@@ -46,37 +54,35 @@ public class Search{
 
 
 
+    public void showQRCode(String string, SearchPlayerCallback callback){
+        ArrayList<String> qrcodes = new ArrayList<>();
+        Query query = codes.whereGreaterThanOrEqualTo(FieldPath.documentId(), string)
+                .orderBy(FieldPath.documentId())
+                .limit(10);
+
+        query.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                for (QueryDocumentSnapshot document : task.getResult()) {
+                    String id = document.getId() ;
+                    if (id.startsWith(string)) {
+                        qrcodes.add(id + ": " + document.getLong("score") + "pts");
+                    }
+                }
+            } else {
+                System.out.println("NOOOOOOOOOOOOOO");
+                Log.e("TAG", "Error getting documents: ", task.getException());
+            }
+            callback.onSearchPlayerComplete(qrcodes);
+        });
+    }
 
 
 
-//    /**
-//     * This method searches for the players with the username the user searches for
-//     * @param string the username user inputs
-//     * @return a list of usernames and their scores
-//     */
-//    public ArrayList searchPlayer(String string){
-////        ArrayList<String> usernames = new ArrayList<>();
-//        Query query = player.whereGreaterThanOrEqualTo(FieldPath.documentId(), string)
-//                .orderBy(FieldPath.documentId())
-//                .limit(10);
-//
-//        query.get().addOnCompleteListener(task -> {
-//            if (task.isSuccessful()) {
-//                for (QueryDocumentSnapshot document : task.getResult()) {
-//                    String id = document.getId() ;
-//                    if (id.startsWith(string)) {
-//                        usernames.add(id + ": " + document.getLong("score") + "pts");
-//                    }
-//                }
-//            } else {
-//                System.out.println("NOOOOOOOOOOOOOO");
-//                Log.e("TAG", "Error getting documents: ", task.getException());
-//            }
-//            System.out.println(usernames);
-//        });
-////        System.out.println(usernames);
-//        return null;
-//    }
+
+
+
+
+
 
 
 }
