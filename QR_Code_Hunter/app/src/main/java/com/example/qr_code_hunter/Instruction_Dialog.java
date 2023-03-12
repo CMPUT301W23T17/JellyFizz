a4c029ee0d7372fa7cb80a391e111179442cdb20
kyle-quach <109721786+kyle-quach@users.noreply.github.com>
2023-03-12 00:48:01 -0700

    Instruction_Dialog Finish

--- QR_Code_Hunter/app/src/main/java/com/example/qr_code_hunter/Instruction_Dialog.java
+++ QR_Code_Hunter/app/src/main/java/com/example/qr_code_hunter/Instruction_Dialog.java
@@ -4,6 +4,9 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatDialogFragment;
 
@@ -11,16 +14,30 @@ public class Instruction_Dialog extends AppCompatDialogFragment {
     public Dialog onCreateDialog (Bundle savedInstanceState){
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
+        // Get the layout inflater
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
 
+        // Inflate the layout for the dialog
+        View view = inflater.inflate(R.layout.instruction_dialog, null);
 
+//        // Customize the dialog UI
+//        TextView titleTextView = view.findViewById(R.id.title);
+//        titleTextView.setText("Enter your name");
 
+        // Add the view to the dialog builder
+        builder.setView(view);
 
-
-
-
-
-
-
+//        // Add action buttons
+//        builder.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int id) {
+//                        // User clicked OK button
+//                    }
+//                })
+//                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int id) {
+//                        // User cancelled the dialog
+//                    }
+//                });
 
 //        // Set the dialog title and message
 //        builder.setTitle("My Dialog")
