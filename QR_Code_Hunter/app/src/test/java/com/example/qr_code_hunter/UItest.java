5e6ede735a6f632b5058e4949d0305b9c4c03de4
hrishit-boop <73561430+hrishit-boop@users.noreply.github.com>
2023-03-15 15:32:29 -0600

    Update UItest.java

--- QR_Code_Hunter/app/src/test/java/com/example/qr_code_hunter/UItest.java
+++ QR_Code_Hunter/app/src/test/java/com/example/qr_code_hunter/UItest.java
@@ -11,14 +11,12 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.is;
 
 /**
  * This is a class that tests the various elements of the UI
