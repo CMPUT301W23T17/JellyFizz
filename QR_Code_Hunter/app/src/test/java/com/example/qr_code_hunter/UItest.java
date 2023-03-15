e8de60129a4c13030c08832a0b335c3e69a722fa
hrishit-boop <73561430+hrishit-boop@users.noreply.github.com>
2023-03-15 15:29:56 -0600

    Update UItest.java

--- QR_Code_Hunter/app/src/test/java/com/example/qr_code_hunter/UItest.java
+++ QR_Code_Hunter/app/src/test/java/com/example/qr_code_hunter/UItest.java
@@ -2,7 +2,7 @@ package com.example.qr_code_hunter;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * This is a class that tests the various elements of the UI
