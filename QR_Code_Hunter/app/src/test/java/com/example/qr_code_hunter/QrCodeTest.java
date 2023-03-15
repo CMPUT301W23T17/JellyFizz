46b6db5d4ad64e8092d5368d1621a771b367aace
hrishit-boop <73561430+hrishit-boop@users.noreply.github.com>
2023-03-15 15:20:20 -0600

    Update QrCodeTest.java

--- QR_Code_Hunter/app/src/test/java/com/example/qr_code_hunter/QrCodeTest.java
+++ QR_Code_Hunter/app/src/test/java/com/example/qr_code_hunter/QrCodeTest.java
@@ -39,26 +39,6 @@ public class QrCodeTest {
     }
     
     @Test
-    public void testInstructionButton() {
-        // First, find the instruction button and welcome owner text view
-        ImageButton instructionButton = (ImageButton) activityRule.getActivity().findViewById(R.id.ask_button);
-        TextView welcomeOwner = (TextView) activityRule.getActivity().findViewById(R.id.welcome_user);
-
-        // Set the owner name to a known value
-        String ownerName = "John";
-        loginActivity.setOwnerName(ownerName);
-
-        // Click on the instruction button
-        instructionButton.performClick();
-
-        // Wait for the dialog to appear
-        onView(withText("Instructions")).inRoot(isDialog()).check(matches(isDisplayed()));
-
-        // Check that the welcome owner text view displays the correct message
-        onView(withId(R.id.welcome_user)).check(matches(withText("WELCOME " + ownerName)));
-    }
-    
-    @Test
     public void testSetPrivacy() {
         QrCode qrCode = new QrCode();
         qrCode.setPrivacy(true);
