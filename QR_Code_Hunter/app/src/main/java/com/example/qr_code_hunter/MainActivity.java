4062159b60f3b0452b44c0186a8b6ac99d8dc54d
QuinNguyen02 <109224012+QuinNguyen02@users.noreply.github.com>
2023-03-17 23:08:39 -0600

    update test code

--- QR_Code_Hunter/app/src/main/java/com/example/qr_code_hunter/MainActivity.java
+++ QR_Code_Hunter/app/src/main/java/com/example/qr_code_hunter/MainActivity.java
@@ -23,10 +23,12 @@ public class MainActivity extends AppCompatActivity {
     private final CollectionReference player = db.collection("Players");
     private final CollectionReference qrcode = db.collection("QrCodes");
     private final CollectionReference scanned = db.collection("scannedBy");
-
+    private String image;
     private QrCode newCode;
+    private QrCode newCode2;
 
     Button btn;
+    Button btn2;
     DocumentReference justScan;
 
     ArrayList<DocumentReference> docRefs = new ArrayList<>();
@@ -35,39 +37,63 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Owner Elizabeth = new Owner("18927841233","elizabeth@ualberta.ca","Elizabeth", false, docRefs, 0, 0);
+        Owner Quin = new Owner("QuinNguyen","elizabeth@ualberta.ca","Daniel", false, docRefs, 40, 3,2,30);
+        Owner Elizabeth = new Owner("Elizabeth","elizabeth@ualberta.ca","Elizabeth", false, docRefs, 30, 1,1,30);
 
         try {
-            newCode = new QrCode("Monkey");
+            newCode = new QrCode("Surprise");
+            newCode2 = new QrCode("Disappointment");
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
 
-        LatLng location = new LatLng(53.52346,-113.5279);
+        LatLng location = new LatLng(53.542061,-113.489435);
+        LatLng location2 = new LatLng(53.542482, -113.491763);
+
         newCode.setLocation(location);
-        newCode.setPrivacy(false);
+        newCode2.setLocation(location2);
 
         btn = findViewById(R.id.button);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Elizabeth.checkQrCodeExist(newCode.getHashString(), new Owner.CheckExistCallback() {
+                Quin.checkQrCodeExist(newCode2.getHashString(), new Owner.CheckExistCallback() {
                     @Override
                     public void onCheckExitedComplete(DocumentReference existQrRef) {
                         if (existQrRef != null) {
-                            Elizabeth.checkDuplicateCodeScanned(existQrRef, new Owner.CheckDuplicateCallback() {
+                            Toast.makeText(MainActivity.this, Quin.getUsername(),Toast.LENGTH_SHORT).show();
+                            Quin.checkDuplicateCodeScanned(existQrRef, new Owner.CheckDuplicateCallback() {
                                 @Override
                                 public void onCheckDuplicateComplete(Boolean duplicated) {
                                     if(!duplicated) {
                                         Toast.makeText(MainActivity.this, "Not Duplicated",Toast.LENGTH_SHORT).show();
-                                        Elizabeth.addQRCode(newCode,"Context is Monkey","2A2A");
+                                        Quin.addQRCode(newCode2, "Braun add","hihi");
                                     } else {
                                         Toast.makeText(MainActivity.this, "Duplicated",Toast.LENGTH_SHORT).show();
-                                    }  
+                                    }
                                 }
                             });
                         } else {
-                            Elizabeth.addQRCode(newCode,"Context is Monkey","2A2A");
+                            Toast.makeText(MainActivity.this, "Not existed",Toast.LENGTH_SHORT).show();
+                            Quin.addQRCode(newCode2,"Braun add","hihi");
+                        }
+                    }
+                });
+            }
+        });
+
+        btn2 = findViewById(R.id.button2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Quin.checkQrCodeExist(newCode.getHashString(), new Owner.CheckExistCallback() {
+                    @Override
+                    public void onCheckExitedComplete(DocumentReference existQrRef) {
+                        if (existQrRef != null) {
+                            Toast.makeText(MainActivity.this, "existed",Toast.LENGTH_SHORT).show();
+                            Quin.deleteQRCode(newCode.getHashString(),10,20);
+                        } else {
+                            Toast.makeText(MainActivity.this, "Not existed",Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
