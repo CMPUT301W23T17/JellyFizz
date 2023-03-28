8f7d323e0e20e21d0c4367b055f98e95f6d476f8
hrishit-boop <73561430+hrishit-boop@users.noreply.github.com>
2023-03-27 18:10:16 -0600

    Update HomePageTest.java

--- QR_Code_Hunter/app/src/androidTest/java/com/example/qr_code_hunter/HomePageTest.java
+++ QR_Code_Hunter/app/src/androidTest/java/com/example/qr_code_hunter/HomePageTest.java
@@ -15,6 +15,14 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+
 @RunWith(AndroidJUnit4.class)
 public class HomepageFragmentTest {
 
