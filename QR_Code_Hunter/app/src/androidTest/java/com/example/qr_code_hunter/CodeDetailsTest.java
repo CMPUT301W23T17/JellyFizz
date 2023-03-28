086d89bc84ffd77fe854a2e5b1b21a5190df3003
hrishit-boop <73561430+hrishit-boop@users.noreply.github.com>
2023-03-27 18:06:43 -0600

    Update CodeDetailsTest.java

--- QR_Code_Hunter/app/src/androidTest/java/com/example/qr_code_hunter/CodeDetailsTest.java
+++ QR_Code_Hunter/app/src/androidTest/java/com/example/qr_code_hunter/CodeDetailsTest.java
@@ -6,6 +6,11 @@ import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 
