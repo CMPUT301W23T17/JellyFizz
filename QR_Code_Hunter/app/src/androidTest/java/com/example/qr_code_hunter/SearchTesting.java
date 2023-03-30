29ea7f33d8c6115006e93f83b20fb94b47402e22
Daniel Akanmu <inioluwa.akanmu@gmail.com>
2023-03-30 00:16:55 -0600

    Fixed other player fragment

--- QR_Code_Hunter/app/src/androidTest/java/com/example/qr_code_hunter/SearchTesting.java
+++ QR_Code_Hunter/app/src/androidTest/java/com/example/qr_code_hunter/SearchTesting.java
@@ -1,4 +1,94 @@
 package com.example.qr_code_hunter;
 
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.robotium.solo.Solo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.CompletableFuture;
+
+@RunWith(AndroidJUnit4.class)
 public class SearchTesting {
+    private Solo soloLogin;
+    private Solo soloMain;
+    static FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+    String user = "DanTestSearch";
+
+
+    @Rule
+    public ActivityTestRule<loginActivity> logInRule = new ActivityTestRule<>(loginActivity.class, true, true);
+
+    public ActivityTestRule<MainActivity> mainActivityRule =
+            new ActivityTestRule<>(MainActivity.class, true, true);
+
+
+    /**
+     * Runs before all tests and creates solo instance.
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        soloLogin = new Solo(InstrumentationRegistry.getInstrumentation(), logInRule.getActivity());
+        soloMain = new Solo(InstrumentationRegistry.getInstrumentation(), mainActivityRule.getActivity());
+        // Enter valid user details
+        soloLogin.enterText(0, user);
+        soloLogin.enterText(1, "akanmu@ualberta.ca");
+        soloLogin.enterText(2, "6045556795");
+        // Click register button
+        soloLogin.clickOnButton("Register");
+
+        // Wait
+        soloMain.waitForView(R.id.scan_now);
+        // Click register button
+        soloMain.clickOnView(soloMain.getView(R.id.search_screen));
+        //solo.waitForView(solo.getView(R.id.buttonTotalScore));
+    }
+
+    @After
+    public void cleanup() throws InterruptedException {
+        CompletableFuture completeDelete1 = new CompletableFuture();
+
+        db.collection("Players").document(user).delete()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        System.out.println("Document successfully deleted");
+                    } else {
+                        System.out.println("Error deleting document: " + task.getException());
+                    }
+                    completeDelete1.complete(null);
+                });
+        completeDelete1.join();
+    }
+
+    @Test
+    public void testSearchScreen() throws Exception {
+        soloMain.waitForView(R.id.search_bar);
+        // Verify that the loginActivity is displayed
+        onView(withId(R.id.search_screen)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testSearching() throws Exception {
+        soloMain.waitForView(R.id.search_bar);
+//        // Verify that the loginActivity is displayed
+        soloMain.enterText(0,"D");
+//        onView(withId(R.id.search_screen)).check(matches(isDisplayed()));
+    }
+
 }
