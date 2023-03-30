c28e6fc6d4f5295215c6ce8ecd01730f9b0f373c
hrishit-boop <73561430+hrishit-boop@users.noreply.github.com>
2023-03-30 14:14:54 -0600

    Update HomePageFragmentTest.java

--- QR_Code_Hunter/app/src/androidTest/java/com/example/qr_code_hunter/HomePageFragmentTest.java
+++ QR_Code_Hunter/app/src/androidTest/java/com/example/qr_code_hunter/HomePageFragmentTest.java
@@ -3,7 +3,10 @@ package com.example.qr_code_hunter;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -23,22 +26,64 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.robotium.solo.Solo;
+
+import java.util.concurrent.CompletableFuture;
+
 @RunWith(AndroidJUnit4.class)
 public class HomepageFragmentTest {
 
-    @Rule
-    public ActivityScenarioRule<MainActivity> activityScenarioRule =
-            new ActivityScenarioRule<>(MainActivity.class);
+    private Solo soloLogin;
+    private Solo soloMain;
+    static FirebaseFirestore db = FirebaseFirestore.getInstance();
 
+
+    // Change this username until it's unique
+    String user = "TestRanking";
+    @Rule
+    public ActivityTestRule<loginActivity> logInRule = new ActivityTestRule<>(loginActivity.class, true, true);
+
+    public ActivityTestRule<MainActivity> mainActivityRule =
+            new ActivityTestRule<>(MainActivity.class, true, true);
+
+
+    /**
+     * Runs before all tests and creates solo instance.
+     * @throws Exception
+     */
     @Before
-    public void launchFragment() {
-        ActivityScenario scenario = activityScenarioRule.getScenario();
-        scenario.onActivity(activity -> {
-            HomepageFragment homepageFragment = new HomepageFragment();
-            activity.getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.nav_host_fragment, homepageFragment, "homepage_fragment")
-                    .commit();
-        });
+    public void setUp() throws Exception {
+        soloLogin = new Solo(InstrumentationRegistry.getInstrumentation(), logInRule.getActivity());
+        soloMain = new Solo(InstrumentationRegistry.getInstrumentation(), mainActivityRule.getActivity());
+        // Enter valid user details
+        soloLogin.enterText(0, user);
+        soloLogin.enterText(1, "tmquach@ualberta.ca");
+        soloLogin.enterText(2, "6043765432");
+        // Click register button
+        soloLogin.clickOnButton("Register");
+
+        // Wait
+        soloMain.waitForView(R.id.scan_now);
+        // Click register button
+        soloMain.clickOnView(soloMain.getView(R.id.home_screen));
+        //solo.waitForView(solo.getView(R.id.buttonTotalScore));
+    }
+
+    @After
+    public void cleanup() throws InterruptedException {
+        CompletableFuture completeDelete1 = new CompletableFuture();
+
+        db.collection("Players").document(user).delete()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        System.out.println("Document successfully deleted");
+                    } else {
+                        System.out.println("Error deleting document: " + task.getException());
+                    }
+                    completeDelete1.complete(null);
+                });
+        completeDelete1.join();
     }
 
     @Test
