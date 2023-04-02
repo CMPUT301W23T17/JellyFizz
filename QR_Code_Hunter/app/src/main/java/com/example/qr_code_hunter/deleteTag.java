a4cc078735ba2afdbe456764f9278b6f52bb7698
unknown <mattneba4343@gmail.com>
2023-04-01 22:35:51 -0600

    Finished adding javadocs

--- QR_Code_Hunter/app/src/main/java/com/example/qr_code_hunter/deleteTag.java
+++ QR_Code_Hunter/app/src/main/java/com/example/qr_code_hunter/deleteTag.java
@@ -2,8 +2,12 @@ package com.example.qr_code_hunter;
 
 import com.google.firebase.firestore.DocumentReference;
 
+/**
+ * This class is a wrapper class for the hashString of a qrCode. It marks which documents need to be deleted
+ */
 public class deleteTag {
     private DocumentReference hashString;
+
     private boolean isChecked;
 
     public DocumentReference getHashString() {
